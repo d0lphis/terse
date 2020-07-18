be3bb0763022c81a17c1064ab0c954ab34a59893
Will Xian <willxian@gmail.com>
2020-07-18 16:15:57 +0800

    Support comparison: between jars, between tars.

--- archive_util/jar_compare.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#if two jars are different, ouptut diff
-#if two jars are the same, ouptut nothing
-
-a=$(unzip -l $1 | tr -s ' ' | cut -d ' ' -f 5 | sort | uniq);
-b=$(unzip -l $2 | tr -s ' ' | cut -d ' ' -f 5 | sort | uniq);
-diff <(echo "$a") <(echo "$b")
