a09883a5ce608ae3eae354dc8c14fb86ab06895a
Will Xian <willxian@gmail.com>
2021-01-22 16:26:27 +0800

    Add compress remote copy script.

---
+++ auto_spawn/ahypersync.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/expect -f
+# exp_internal 1    ;# uncomment to turn on expect debugging
+
+# usage: ./ahypersync.sh 8.30.204.204 "" root password /wks/kode/wksp/project/subprojects/db/build/ reports /mnt/
+#        this will go to remote /wks/kode/wksp/project/subprojects/db/build/, compress folder reports to reports.tar.gz, then rsync it back as local /mnt/reports.tar.gz
+#       ./ahypersync.sh "" 8.30.204.204 root password /mnt/ reports /wks/kode/wksp/project/subprojects/db/build/
+#        this will got to local /mnt/, compress folder reports to reports.tar.gz, then rsync it to remote as local /wks/kode/wksp/project/subprojects/db/build/reports.tar.gz
+
+
+
+set source_hostname [lindex $argv 0]
+set destination_hostname [lindex $argv 1]
+set username [lindex $argv 2]
+set password [lindex $argv 3]
+set source_folder_path [lindex $argv 4]
+set source_folder_name [lindex $argv 5]
+set destination_folder_path [lindex $argv 6]
+
+
+set timeout -1
+
+if { $source_hostname != "" } {
+  spawn ssh $username@$source_hostname "pushd $source_folder_path && rm -rf ${source_folder_name}.tar.gz && tar -cvzf ${source_folder_name}.tar.gz $source_folder_name && popd"
+} else {
+  pushd $source_folder_path && rm -rf ${source_folder_name}.tar.gz && tar -cvzf ${source_folder_name}.tar.gz $source_folder_name && popd
+}
+
+expect {
+  "*continue connecting (yes/no)?*" {
+    send "yes\r"
+    expect {
+      "*assword*" {
+        send "$password\r"
+      }
+    }
+  }
+  "*assword*" {
+    send "$password\r"
+  }
+}
+expect EOF
+
+
+if { $source_hostname != "" } {
+  spawn rsync -av --progress $username@$source_hostname:$source_folder_path/${source_folder_name}.tar.gz $destination_folder_path
+} else {
+  spawn rsync -av --progress $source_folder_path/${source_folder_name}.tar.gz $username@$destination_hostname:$destination_folder_path
+}
+
+expect {
+  "*continue connecting (yes/no)?*" {
+    send "yes\r"
+    expect {
+      "*assword*" {
+        send "$password\r"
+      }
+    }
+  }
+  "*assword*" {
+    send "$password\r"
+  }
+}
+expect EOF
